d21530578c27d3d0dd221c2b4c10e0e8c9a3cf19
sunshanpeng <sunshanpeng@outlook.com>
2023-02-14 21:16:46 +0800

    init

---
+++ armoury-user/armoury-user-server/src/main/kotlin/com/sunshanpeng/armoury/user/ArmoryUserServerApplication.kt
@@ -0,0 +1,11 @@
+package com.sunshanpeng.armoury.user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArmouryUserServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArmouryUserServerApplication>(*args)
+}
---
+++ armoury-user/armoury-user-server/src/test/kotlin/com/sunshanpeng/armoury/user/ArmoryUserServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sunshanpeng.armoury.user
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class armouryUserServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
